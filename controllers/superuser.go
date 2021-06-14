94b364f79b44aac19e0e8759e3c507b2a7591756
Nathaniel Suchy <me@nsuchy.me>
2021-06-14 18:23:39 -0400

    Added a SuperUser filter

--- controllers/superuser.go
+++ controllers/superuser.go
@@ -7,12 +7,12 @@ import (
 	beego "github.com/beego/beego/v2/server/web"
 )
 
-type PrivateController struct {
-	BaseController
+type SuperUserController struct {
+	PrivateController
 }
 
 // Runs after Init before request function execution
-func (c *PrivateController) Prepare() {
+func (c *SuperUserController) Prepare() {
 	c.Data["IsSignedIn"] = (c.GetSession("user_id") != nil)
 	c.Data["IsNotSignedIn"] = (c.GetSession("user_id") == nil)
 
@@ -41,11 +41,20 @@ func (c *PrivateController) Prepare() {
 				flash.Store(&c.Controller)
 				c.Ctx.Redirect(302, "/sign-in")
 			}
+
+			if user.IsSuperUser != true {
+				// The user is signed into a non-superuser account. Destroy their session.
+				c.DestroySession()
+				flash := beego.NewFlash()
+				flash.Warning("The account you were logged into is not a superuser. Please sign into an account with superuser status!")
+				flash.Store(&c.Controller)
+				c.Ctx.Redirect(302, "/sign-in")
+			}
 		}
 	}
 }
 
 // Runs after request function execution
-func (c *PrivateController) Finish() {
+func (c *SuperUserController) Finish() {
 	// Any cleanup logic common to all requests goes here. Logging or metrics, for example.
 }
